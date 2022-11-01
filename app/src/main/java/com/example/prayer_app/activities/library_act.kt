5464c47f7899463e15c64ba18d73ba290fe36b20
Nursultan <nursamanzhol@gmail.com>
2022-11-01 21:19:29 +0600

    +add kitaphana in the used LinearLayout, ScrollView, HorizontalScrollView, CardView

---
+++ app/src/main/java/com/example/prayer_app/activities/library_act.kt
@@ -0,0 +1,12 @@
+package com.example.prayer_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.prayer_app.R
+
+class library_act : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
