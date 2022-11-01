782fe47143736bba32a71364cdff5f90e26f9a4e
Galymzhan Tolepbergen <galymzhan.tolepbergen@gmail.com>
2022-11-01 20:59:40 +0600

    + add zikir count xml + add design for main activity

--- app/src/main/java/com/example/prayer_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/prayer_app/activities/MainActivity.kt
@@ -13,7 +13,7 @@ import com.example.prayer_app.R.layout.quran
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(quran)
+        setContentView(activity_main)
 
 //        val btn = findViewById<Button>(R.id.button)
 //        btn.setOnClickListener {
