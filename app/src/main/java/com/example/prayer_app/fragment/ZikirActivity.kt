98ba3dbc9f0c62f67e09bcd87e3225acd7d259ec
Galymzhan Tolepbergen <galymjan2002g@mail.ru>
2022-11-09 14:14:43 +0600

    HW8- zkCount working

--- app/src/main/java/com/example/prayer_app/fragment/ZikirActivity.kt
+++ app/src/main/java/com/example/prayer_app/fragment/ZikirActivity.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.prayer_app.R
+import org.w3c.dom.Text
+
 
 
 class ZikirActivity : Fragment() {
+    var countZ=0
 
 
     override fun onCreateView(
@@ -16,8 +21,25 @@ class ZikirActivity : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_zikir_activity, container, false)
+
+        // hw8
+        val view = inflater.inflate(R.layout.fragment_zikir_activity, container, false)
+        var zkCount: Text = view.findViewById(R.id.zkCount)
+        val btnCount: Button = view.findViewById(R.id.btnCount)
+        zkCount.text=countZ.toString()
+
+        btnCount.setOnClickListener{
+            ++countZ
+            println("clicked button 2")
+            Toast.makeText(view?.context, "Button Clicked", Toast.LENGTH_LONG).show()
+            zkCount.text=countZ.toString()
+
+        }
+        //hw8
+        return view
+      //  return inflater.inflate(R.layout.fragment_zikir_activity, container, false)
     }
+
 
 
 }
